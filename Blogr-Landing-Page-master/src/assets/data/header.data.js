5e191e28844fd15fe31e0b8e1e4003e19c3a7677
Arindam Sankar Das <arindamsankar@outlook.com>
2024-02-06 08:39:59 +0530

    Delete Blogr-Landing-Page-master directory

--- Blogr-Landing-Page-master/src/assets/data/header.data.js
+++
@@ -1,7 +0,0 @@
-const HEADER_DATA = {
-  links: ["Product", "Company", "Connect"],
-  buttons: ["Login", "Sign Up"],
-  title_buttons: ["Start for Free", "Learn More"],
-};
-
-export default HEADER_DATA;
