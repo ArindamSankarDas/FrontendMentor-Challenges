ce5286f9835ac9dd7cc3eb941292605061b7b017
Arindam Sankar Das <arindamsankar@outlook.com>
2024-02-06 08:40:55 +0530

    deleted a outdated directory

--- Blogr-Landing-Page-master/src/Components/Buttons/medium-Button/md-button.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./md.button.styles.scss";
-
-const MediumButton = ({ children }) => {
-  return <button className="btn_md">{children}</button>;
-};
-
-export default MediumButton;
